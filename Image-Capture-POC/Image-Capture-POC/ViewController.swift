bd7d484f30df6dcb896e3446abbf52eca7e13d8f
Bryan Luna <lunabryan983@gmail.com>
2025-09-23 22:37:39 -0600

    feat: Base project created

---
+++ Image-Capture-POC/Image-Capture-POC/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Image-Capture-POC
+//
+//  Created by Jose Luna on 9/23/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
